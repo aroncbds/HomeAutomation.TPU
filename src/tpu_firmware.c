8226d98bf40a4379563c5b0198bd7517980042eb
git <aron.kelemen@stralfors.se>
2025-02-20 23:17:14 +0100

    Print sensor values

--- src/tpu_firmware.c
+++ src/tpu_firmware.c
@@ -34,11 +34,47 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
 EthernetServer server(80);
 
+// Trying to fetch device addresses of temperature probes (perhaps we need to create a one-off application for this purpose)
+DeviceAddress Thermometer;  // Variable to hold device address (https://lastminuteengineers.com/multiple-ds18b20-arduino-tutorial/)
+int deviceCount = 0;
+
+void printAddress(DeviceAddress deviceAddress)
+{ 
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.print("0x");
+    if (deviceAddress[i] < 0x10) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+    if (i < 7) Serial.print(", ");
+  }
+  Serial.println("");
+}
+
 void setup() {
 
     Serial.begin(115200);
 
     sensors.begin();
+
+
+    // // locate devices on the bus
+    // Serial.println("Locating devices...");
+    // Serial.print("Found ");
+    // deviceCount = sensors.getDeviceCount();
+    // Serial.print(deviceCount, DEC);
+    // Serial.println(" devices.");
+    // Serial.println("");
+    
+    // Serial.println("Printing addresses...");
+    // for (int i = 0;  i < deviceCount;  i++)
+    // {
+    //   Serial.print("Sensor ");
+    //   Serial.print(i+1);
+    //   Serial.print(" : ");
+    //   sensors.getAddress(Thermometer, i);
+    //   printAddress(Thermometer);
+    // }
+
 
     snprintfMutex = xSemaphoreCreateMutex();
     if (snprintfMutex == NULL) {
@@ -96,13 +132,19 @@ void TaskWebServer(void *pvParameters)
       }
 
       // Serve temperature JSON response
-      if (request.indexOf("GET /temperature") >= 0) {
+      if (request.indexOf("GET /temperatures") >= 0) {
         client.println("HTTP/1.1 200 OK");
         client.println("Content-Type: application/json");
         client.println("Connection: close");
         client.println();
-        client.print("{\"temperature\":");
+        client.print("{\"t1t\":");
         client.print(temperatureT1T);
+        client.print(",");
+        client.print("\"t1m\":");
+        client.print(temperatureT1M);
+        client.print(",");
+        client.print("\"t2t\":");
+        client.print(tempProbe);
         client.println("}");
       }
 
